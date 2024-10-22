7dba6e3c9abe103fe4707281ea70505030a5b913
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-10-21 21:30:23 -0700

    auto update

--- upload.py
+++ upload.py
@@ -2,8 +2,33 @@ import os
 import shutil
 import json
 from tqdm import tqdm
+import subprocess
 
 BATCH_SIZE = 40
+
+
+def get_latest_commit():
+    # Run the git log command to get the latest commit hash
+    result = subprocess.run(
+        ["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, text=True
+    )
+
+    # Strip any trailing newline or spaces
+    latest_commit = result.stdout.strip()
+    return latest_commit
+
+
+def get_modified_and_added_files(commit1, commit2):
+    # Run the git diff command with --diff-filter=AM to ignore deleted files
+    result = subprocess.run(
+        ["git", "diff", "--name-only", "--diff-filter=AM", commit1, commit2],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+
+    # Split the result by newline to get individual file names
+    files = result.stdout.splitlines()
+    return files
 
 
 def get_all_files_in_dir_recursively(dirname):
@@ -47,14 +72,38 @@ def commit_single_file(file_path):
     print(file_path + " uploaded successfully!")
 
 
+def filter_card_only(files):
+    card_numbers = []
+    for file in files:
+        number_str = file.split(os.path.sep)[-1].split(".")[0]
+        # see if it can be converted to an integer
+        number = None
+        try:
+            number = int(number_str)
+            card_numbers.append(number)
+        except:
+            continue
+    return card_numbers
+
+
 if __name__ == "__main__":
+    last_commit_hash = get_latest_commit()
 
     commit_single_file("update.txt")
-    commit_single_file("update.json")
-
     if not os.path.exists("output"):
         print("output directory does not exist!")
         exit(-1)
     all_files = get_all_files_in_dir_recursively("output")
     commit_and_push(all_files)
     print("All files uploaded successfully!")
+
+    now_commit_hash = get_latest_commit()
+    modified_files = get_modified_and_added_files(last_commit_hash, now_commit_hash)
+
+    modified_cards = filter_card_only(modified_files)
+    print("Modified cards: ", modified_cards)
+
+    with open("update.json", "w") as f:
+        json.dump(modified_cards, f)
+    commit_single_file("update.json")
+    print("upload completed")
